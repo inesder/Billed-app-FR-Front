573490bb7cb4e5a5da59b1d14564a29cca7d40e8
inesder <inesderraz@hotmail.fr>
2024-06-26 19:18:03 +0200

    fix showing tickets issue

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -131,26 +131,33 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0
-    if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
-    } else {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
-      this.counter ++
+    // Initialiser expandedLists si ce n'est pas déjà fait
+    if (!this.expandedLists) {
+      this.expandedLists = {}; 
     }
-
+    
+    // Initialiser l'état de la liste si ce n'est pas déjà fait
+    if (this.expandedLists[index] === undefined) {
+      this.expandedLists[index] = false; 
+    }
+  
+    // Inverser l'état de la liste
+    this.expandedLists[index] = !this.expandedLists[index];
+  
+    // Mettre à jour l'affichage en fonction de l'état
+    if (this.expandedLists[index]) {
+      $(`#arrow-icon${index}`).css({ transform: 'rotate(0deg)' });
+      $(`#status-bills-container${index}`).html(cards(filteredBills(bills, getStatus(index))));
+    } else {
+      $(`#arrow-icon${index}`).css({ transform: 'rotate(90deg)' });
+      $(`#status-bills-container${index}`).html("");
+    }
+  
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
-
-    return bills
-
+      $(`#open-bill${bill.id}`).off('click').on('click', (e) => this.handleEditTicket(e, bill, bills));
+    });
+  
+    return bills;
   }
 
   getBillsAllUsers = () => {
