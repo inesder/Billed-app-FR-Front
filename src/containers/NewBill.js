fc507f3d308d09b9c52ebf25aa2a677a46bd5e4c
inesder <inesderraz@hotmail.fr>
2024-07-04 18:48:11 +0200

    fix format image

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -17,14 +17,21 @@ export default class NewBill {
   }
   handleChangeFile = e => {
     e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const fileInput = this.document.querySelector(`input[data-testid="file"]`)
+    const file = fileInput.files[0]
+    // Vérification du format de l'image
+    const allowedFormats = ['image/jpeg', 'image/png', 'image/jpg']
+    if (!allowedFormats.includes(file.type)){
+        alert('Seuls les formats JPEG, JPG et PNG sont autorisés.')
+          fileInput.value = null  // Réinitialiser la valeur du champ de fichier
+          return
+      }
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
     formData.append('email', email)
-
     this.store
       .bills()
       .create({
